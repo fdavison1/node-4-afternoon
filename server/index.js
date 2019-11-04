698464b3fbcef955975cc6f879e5f09036299688
Fred Davison <fredbook@Freds-MacBook-Pro.local>
2019-11-04 14:12:08 -0700

    step 10

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const {SERVER_PORT, SESSION_SECRET} = process.env
 const middleware = require('./middleware/checkForSession')
 const swagCTRL = require('./controllers/swagController')
 const authCTRL = require('./controllers/authController')
+const cartCTRL = require('./controllers/cartController')
 
 const app = express()
 
@@ -17,13 +18,18 @@ app.use(session({
 }))
 app.use(middleware.sessionCheck)
 
-//endpoints
+//endpoints:
+//SWAG CTRL
 app.get('/api/swag', swagCTRL.read)
-
+//AUTH CTRL
 app.post('/api/login', authCTRL.login)
 app.post('/api/register', authCTRL.register)
 app.post('/api/signout', authCTRL.signout)
 app.get('/api/user', authCTRL.getUser)
+//CART CTRL
+app.post('/api/cart/checkout', cartCTRL.checkout)
+app.post('/api/cart:id', cartCTRL.add)
+app.delete('/api/cart:id', cartCTRL.delete)
 
 //listening
 app.listen(SERVER_PORT, ()=> console.log(`Port ${SERVER_PORT} is on and ready to copy`))
